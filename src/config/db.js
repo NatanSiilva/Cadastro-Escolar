5aa9939322d3600c63736288cc49adbf29869faf
Natanael Silva <natan.progjs@gmail.com>
2021-01-22 23:38:20 -0300

    ataualization 05

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,12 @@
+require('dotenv/config')
+
 const { Pool} = require('pg')
 
 module.exports = new Pool ({
-    user: 'natanProg',
-    password: '153306',
-    host: 'localhost',
-    port: 5432,
-    database: 'cadastroescolar'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    database_url: process.env.DATABASE_URL
 })
--- src/server.js
+++ src/server.js
@@ -1,11 +1,26 @@
+require('dotenv/config')
+
+
 const express = require('express')
 const nunjucks = require('nunjucks')
 const routes = require("./routes")
 const methodOverride = require('method-override')
+const { Client } = require('pg');
 
 const server = express()
 
-server.use(express.urlencoded({ extendens: true }))
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+
+server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'))
 server.use(methodOverride('_method'))
 server.use(routes)
@@ -19,6 +34,6 @@ nunjucks.configure("src/app/views", {
 })
 
 
-server.listen(5000, (req, res) => {
+server.listen(process.env.PORT || 3333, (req, res) => {
     console.log("server is running")
 })
